e02a964fb9bc5f82890a94be281f191ec7976c9f
Untone <anton.rewin@gmail.com>
2021-07-02 10:31:30 +0300

    minor fixes

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -1,4 +1,14 @@
 from resolvers.auth import sign_in, sign_out, register
-from resolvers.inbox import create_message
+from resolvers.inbox import create_message, delete_message, update_message, get_messages
 
-__all__ = ["sign_in", "sign_out", "register"]
+__all__ = [
+    "sign_in",
+    "sign_out",
+    "register",
+    # TODO: "reset_password_code",
+    # TODO: "reset_password_confirm"
+    "create_message",
+    "delete_message",
+    "get_messages",
+    "update_message"
+    ]
