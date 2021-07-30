4fd61c09c4c027fb8568270d074240b761cd0552
Untone <anton.rewin@gmail.com>
2021-07-30 10:14:47 +0300

    validation optional username

--- auth/validations.py
+++ auth/validations.py
@@ -21,7 +21,7 @@ class PayLoad(BaseModel):
 
 class CreateUser(BaseModel):
     email: Text
-    # username: Optional[Text]
+    username: Optional[Text]
     # age: Optional[int]
     # phone: Optional[Text]
     password: Optional[Text]
