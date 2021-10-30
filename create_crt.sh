b7b0c0a905764fc369f616de85afb1c0c18d5941
Untone <anton.rewin@gmail.com>
2021-10-31 02:42:04 +0300

    pem

--- create_crt.sh
+++ create_crt.sh
@@ -8,3 +8,5 @@ openssl req -newkey rsa:4096 \
             -out discours.crt \
             -keyout discours.key \
             -subj "/C=RU/ST=Moscow/L=Moscow/O=Discours/OU=Site/CN=build.discours.io"
+
+openssl x509 -in discours.crt -out discours.pem -outform PEM
