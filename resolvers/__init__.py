ddb5d9d6433a41522a31863c42c6660a10acc76c
Untone <anton.rewin@gmail.com>
2021-10-31 13:02:10 +0300

    tick

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -1,8 +1,10 @@
 from resolvers.auth import login, sign_out, is_email_free, register, confirm
 from resolvers.inbox import create_message, delete_message, update_message, get_messages
-from resolvers.zine import create_shout, get_shout_by_slug, favorite_shouts, recent_shouts, top_authors, top_shouts_by_rating, top_shouts_by_view
+from resolvers.zine import create_shout, get_shout_by_slug, favorite_shouts, \
+    recent_shouts, top_authors, top_shouts_by_rating, top_shouts_by_view
 from resolvers.profile import get_user_by_slug, get_current_user
-from resolvers.topics import topic_subscribe, topic_unsubscribe, topics_by_author, topics_by_community, topics_by_slugs
+from resolvers.topics import topic_subscribe, topic_unsubscribe, topics_by_author, \
+    topics_by_community, topics_by_slugs
 
 __all__ = [
     "login",
