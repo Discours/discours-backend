ddbff3239cf8fc76b1fc447fac07d307d53f691d
tonyrewin <anton.rewin@gmail.com>
2022-07-29 07:44:02 +0300

    py3fix

--- storages/users.py
+++ storages/users.py
@@ -26,7 +26,7 @@ class UserStorage:
 	async def get_all_users():
 		self = UserStorage
 		async with self.lock:
-			return self.users.values().sort(key=lambda user: user.createdAt)
+			return list(self.users.values()).sort(key=lambda user: user.createdAt)
 
 	@staticmethod
 	async def get_user_by_slug(slug):
