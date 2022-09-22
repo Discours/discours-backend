89173ec802ce6f6565307ad7d86d56daf90d1af0
tonyrewin <anton.rewin@gmail.com>
2022-09-22 19:25:32 +0300

    certs-gen

--- create_crt.sh
+++ create_crt.sh
@@ -5,8 +5,10 @@ openssl req -newkey rsa:4096 \
             -sha256 \
             -days 3650 \
             -nodes \
-            -out discours.crt \
-            -keyout discours.key \
+            -out server.crt \
+            -keyout server.key \
             -subj "/C=RU/ST=Moscow/L=Moscow/O=Discours/OU=Site/CN=newapi.discours.io"
 
-openssl x509 -in discours.crt -out discours.pem -outform PEM
+openssl x509 -in server.crt -out server.pem -outform PEM
+tar cvf server.tar server.crt server.key
+dokku certs:add discoursio-api < server.tar
