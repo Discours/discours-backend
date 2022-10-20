d34260e3365f4fbf9ad9b2c32eaeb1e853792752
Igor Lobanov <igor.lobanov@onetwotrip.com>
2022-10-21 00:05:37 +0200

    auth fix

--- auth/email.py
+++ auth/email.py
@@ -4,26 +4,22 @@ from settings import MAILGUN_API_KEY, MAILGUN_DOMAIN
 
 api_url = "https://api.mailgun.net/v3/%s/messages" % MAILGUN_DOMAIN
 noreply = "discours.io <noreply@%s>" % MAILGUN_DOMAIN
-subject = "Confirm email"
-tmplt = """<html><body>
-    Follow the <a href='%s'>link</a> to authorize
-    </body></html>
-    """
-
 
 async def send_auth_email(user, token):
     try:
-        to = "%s <%s>" % (user.username, user.email)
-        url_with_token = "https://newapi.discours.io/confirm/" + token
+        to = "%s <%s>" % (user.name, user.email)
+        # TODO: i18n
+        subject = "Confirm email"
+        template = "email_confirmation_ru"
+
         response = requests.post(
             api_url,
             auth=("api", MAILGUN_API_KEY),
-            data={
-                "from": noreply,
-                "to": to,
-                "subject": subject,
-                "html": tmplt % url_with_token,
-            },
+            data={"from": noreply,
+                  "to": to,
+                  "subject": subject,
+                  "template": template,
+                  "h:X-Mailgun-Variables": "{ \"token\": \"%s\" }" % token}
         )
         response.raise_for_status()
     except Exception as e:
