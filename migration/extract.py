93f6272c6a2eef9ae5fe2d2fbf6d921a31ce710d
tonyrewin <anton.rewin@gmail.com>
2022-11-12 16:18:39 +0300

    migration-fix

--- migration/extract.py
+++ migration/extract.py
@@ -347,7 +347,7 @@ def prepare_html_body(entry):
 
 
 def extract_html(entry):
-    body_orig = entry.get("body") or ""
+    body_orig = entry.get("body").replace('\(', '(').replace('/)', ')') or ""
     media = entry.get("media", [])
     kind = entry.get("type") or ""
     print("[extract] kind: " + kind)
