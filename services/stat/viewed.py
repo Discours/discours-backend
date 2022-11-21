a05ce719a7a067569268f1b4ec29426d265f874a
tonyrewin <anton.rewin@gmail.com>
2022-11-21 08:18:50 +0300

    need-remigration

--- services/stat/viewed.py
+++ services/stat/viewed.py
@@ -4,6 +4,7 @@ from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 from base.orm import local_session
 from sqlalchemy import func, select
+from orm.shout import ShoutTopic
 from orm.viewed import ViewedEntry
 from ssl import create_default_context
 
@@ -42,6 +43,7 @@ ssl = create_default_context()
 class ViewedStorage:
     lock = asyncio.Lock()
     by_shouts = {}
+    by_topics = {}
     period = 5 * 60  # 5 minutes
     client = None
     transport = None
@@ -68,7 +70,7 @@ class ViewedStorage:
         self = ViewedStorage
         async with self.lock:
             r = self.by_shouts.get(shout_slug)
-            if r:
+            if not r:
                 with local_session() as session:
                     shout_views = 0
                     shout_views_q = select(func.sum(ViewedEntry.amount)).where(
@@ -79,6 +81,16 @@ class ViewedStorage:
                     return shout_views
             else:
                 return r
+
+    @staticmethod
+    async def get_topic(topic_slug):
+        self = ViewedStorage
+        topic_views = 0
+        async with self.lock:
+            topic_views_by_shouts = self.by_topics.get(topic_slug) or {}
+            for shout in topic_views_by_shouts:
+                topic_views += shout
+        return topic_views
 
     @staticmethod
     async def increment(shout_slug, amount=1, viewer='anonymous'):
@@ -93,6 +105,12 @@ class ViewedStorage:
                 session.add(viewed)
                 session.commit()
                 self.by_shouts[shout_slug] = self.by_shouts.get(shout_slug, 0) + amount
+                topics = session.query(ShoutTopic).where(ShoutTopic.shout == shout_slug).all()
+                for t in topics:
+                    tpc = t.topic
+                    if not self.by_topics.get(tpc):
+                        self.by_topics[tpc] = {}
+                    self.by_topics[tpc][shout_slug] = self.by_shouts[shout_slug]
 
     @staticmethod
     async def worker():
