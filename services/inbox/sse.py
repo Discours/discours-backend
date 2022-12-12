420db45580919f01e79e6e9ff95fcd77aabcc72b
tonyrewin <anton.rewin@gmail.com>
2022-12-12 11:17:59 +0300

    fix-async-endpoint

--- services/inbox/sse.py
+++ services/inbox/sse.py
@@ -4,7 +4,7 @@ from resolvers.inbox.messages import message_generator
 from base.exceptions import Unauthorized
 
 
-async def sse_messages(request: Request):
+def sse_messages(request: Request):
     print(f'[SSE] {request.scope}')  # FIXME: DOES NOT HAPPEN TO BE CALLED
     # https://github.com/enisdenjo/graphql-sse/blob/master/PROTOCOL.md
     if request['user']:
