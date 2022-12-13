38219e83c5bc9cafc177b94597149f6121dcbf40
tonyrewin <anton.rewin@gmail.com>
2022-12-13 03:07:47 +0300

    test-sse

--- services/inbox/sse.py
+++ services/inbox/sse.py
@@ -4,11 +4,11 @@ from resolvers.inbox.messages import message_generator
 from base.exceptions import Unauthorized
 
 
-def sse_messages(request: Request):
+async def sse_messages(request: Request):
     print(f'[SSE] {request.scope}')  # FIXME: DOES NOT HAPPEN TO BE CALLED
     # https://github.com/enisdenjo/graphql-sse/blob/master/PROTOCOL.md
     if request['user']:
-        event_generator = message_generator(None, request.scope)
+        event_generator = await message_generator(None, request.scope)
         return EventSourceResponse(event_generator)
     else:
         raise Unauthorized("Please login")
