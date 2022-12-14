96b209055a8ca1f1cf4dd3f74db251a9dbf4d117
tonyrewin <anton.rewin@gmail.com>
2022-12-14 11:35:47 +0300

    fix-testing

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ app = Starlette(
     middleware=middleware,
     routes=routes,
 )
-app.mount("/graphql", GraphQL(
+app.mount("/", GraphQL(
     schema,
     debug=True,
     # websocket_handler=GraphQLTransportWSHandler(
@@ -94,7 +94,7 @@ dev_app = app = Starlette(
     middleware=middleware,
     routes=routes,
 )
-dev_app.mount("/graphql", GraphQL(
+dev_app.mount("/", GraphQL(
     schema,
     debug=True,
     # websocket_handler=GraphQLTransportWSHandler(
