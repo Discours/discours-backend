a32a18893ef6d836210328a8585db98f9262fe49
Alex Kulikov <7394728+alexeyqu@users.noreply.github.com>
2023-08-24 11:31:21 +0100

    init alembic (#74)
    * init alembic
    
    * better
    
    * readme

---
+++ alembic/versions/fe943b098418_init_alembic.py
@@ -0,0 +1,26 @@
+"""init alembic
+
+Revision ID: fe943b098418
+Revises: 
+Create Date: 2023-08-19 01:37:57.031933
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'fe943b098418'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
