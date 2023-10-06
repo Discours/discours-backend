d1366d0b88567eb464eecc91a1ebe86489f248d9
Stepan Vladovskiy <duffokin@gmail.com>
2023-10-06 06:14:24 -0300

    feat: @read about keys

--- services/schema.py
+++ services/schema.py
@@ -13,11 +13,10 @@ query = QueryType()
 
 @query.field("_service")
 def resolve_service(*_):
-    # Load the full SDL from your SDL file
-    with open("schemas/core.graphql", "r") as file:
-        full_sdl = file.read()
-    
-    return {"sdl": full_sdl}
+    print("Inside the _service resolver")
+    # For now, return a placeholder SDL.
+    sdl = "type Query { _service: _Service } type _Service { sdl: String }"
+    return {"sdl": sdl}
     
 mutation = MutationType()
 resolvers = [query, mutation, datetime_scalar]
