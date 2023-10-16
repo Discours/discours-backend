b03ac825b6e8725c1e8a8b4bb6b5bff554ae5210
Untone <anton.rewin@gmail.com>
2023-10-16 19:13:39 +0300

    unread-fix3

--- services/unread.py
+++ services/unread.py
@@ -12,6 +12,6 @@ async def get_total_unread_counter(author_id: int) -> int:
     chats_set = await redis.execute("SMEMBERS", f"chats_by_author/{author_id}")
     unread = 0
     for chat_id in list(chats_set):
-        n = await get_unread_counter(chat_id.decode("utf-8"), author_id)
+        n = await get_unread_counter(chat_id, author_id)
         unread += n
     return unread
